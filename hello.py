314d4f45576a49d8712b911a68253aca37585a16
jhon0498 <https://github.com/Jhon0498/Semana4.git>
2024-11-26 23:39:53 +0000

    Atualizaçao

--- hello.py
+++ hello.py
@@ -1,95 +1,103 @@
 from flask import Flask, request, render_template, session, url_for, redirect, flash
 from datetime import datetime
-
-#Parte SQL
 import os
 from flask_sqlalchemy import SQLAlchemy
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
 from flask_moment import Moment
-#Parte do WTF
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+from flask_migrate import Migrate
 
+# Configuração do app
 app = Flask(__name__)
 
-#parte SQL
+# Configurações do banco de dados
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'chave forte'  # Defina uma chave secreta forte para o WTF Forms
 
+# Inicialização do banco de dados e migrações
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Inicialização do Moment (para manipulação de data/hora)
+moment = Moment(app)
+
+# Modelos do banco de dados
 class Role(db.Model):
     __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True, autoincrement = True )
-    name = db.Column(db.String(64), unique = True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
-        return '<Role %r>' % self.name
+        return f'<Role {self.name}>'
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    username = db.Column(db.String(64), unique = True, index = True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<User {self.username}>'
 
-#Parte do WTF
-app.config['SECRET_KEY'] = 'chave forte'
-
-moment = Moment(app)
-
-#Parte Migrate
-from flask_migrate import Migrate
-migrate = Migrate(app, db)
-
+# Formulário de usuário
 class NameForm(FlaskForm):
-    name = StringField('Qual teu nome?', validators = [DataRequired()])
+    name = StringField('What is your name?', validators=[DataRequired()])
+    role = SelectField('Role?:', coerce=int, choices=[])  # Campo para escolher o role
     submit = SubmitField('Submit')
 
-#Essa rota está para uso do forms
+# Rota para o formulário e cadastro de usuário
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     form = NameForm()
+
+    # Carregar as opções de role para o SelectField
+    form.role.choices = [(role.id, role.name) for role in Role.query.all()]
+
     all_user = User.query.all()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user_role = Role.query.filter_by(name='User').first()
-            user = User(username=form.name.data, role_id = user_role.id)
+            # Obtém o role escolhido pelo usuário no formulário
+            user_role = Role.query.get(form.role.data)
+            user = User(username=form.name.data, role_id=user_role.id)
             db.session.add(user)
             db.session.commit()
             flash('Adicionado novo usuário')
         else:
             flash('Já conheço ele')
+
         session['name'] = form.name.data
         return redirect(url_for('hello_world'))
-    return render_template('formulario.html', form = form, name = session.get('name'), pessoa = all_user)
 
-"""
-@app.route('/')
-def hello_world():
-    name = "eu estou usando o JINJA2!";
-    return render_template('template-base.html', current_time = datetime.utcnow());
-"""
+    return render_template('formulario.html', form=form, name=session.get('name'), pessoa=all_user)
+
+# Rota para exibir detalhes do usuário
 @app.route('/user/<name>')
 def hello_pront(name):
-    name2 = name
-    return render_template('user.html', name = name, pront = 'PT3026931', ins = 'IFSP' , current_time = datetime.utcnow())
+    return render_template('user.html', name=name, pront='PT3026931', ins='IFSP', current_time=datetime.utcnow())
+
+# Rota para tratamento de erro 404
 @app.errorhandler(404)
 def not_found(e):
-    return render_template('404.html', current_time = datetime.utcnow()), 404;
+    return render_template('404.html', current_time=datetime.utcnow()), 404
+
+# Rota para exibir detalhes do contexto da requisição
 @app.route('/contextorequisicao')
 def hello_requisi_detalhes():
     navegador = request.headers.get('User-Agent')
-    Ip_remoto = request.headers.get('X-Forwarded-For')
+    ip_remoto = request.headers.get('X-Forwarded-For')
     host_name = request.headers.get('Host')
-    return render_template('contextorequisicao.html', name = 'Jhonatan', navegador = navegador, IP_cliente = Ip_remoto, host_name = host_name);
+    return render_template('contextorequisicao.html', name='Jhonatan', navegador=navegador, IP_cliente=ip_remoto, host_name=host_name)
 
+# Contexto do shell para facilitar o uso do banco de dados
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
+
+if __name__ == '__main__':
+    app.run(debug=True)
